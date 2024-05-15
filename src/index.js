963b8b5360b9c8f8a7310591a9d2bc64f8ebc95e
Abhinav Dogra <65779864+abhinav69-dogra@users.noreply.github.com>
2024-05-15 10:03:38 +0530

    added jwt token

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const {PORT} = require('./config/serverConfig');
+const bodyParser = require('body-parser');
+const apiRoutes = require('./routes/index');
+
+const UserService = require('./services/user-service');
 
 const app = express()
 
 const prepareAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', apiRoutes);
     app.listen(PORT, () => {
         console.log(`Server Satrted on Port : ${PORT}`);
+        // const repo=new UserRepository();
+        // const res=repo.getById(1);
+        // console.log(res);
+
+        // const service = new UserService();
+        // const newToken = service.createToken({email : 'sanket@admin.com', id : 1});
+        // console.log("new token is", newToken); 
+
     });
 }
 
---
+++ src/services/user-service.js
@@ -0,0 +1,45 @@
+const UserRepository = require('../repository/user-repository');
+const jwt = require('jsonwebtoken');
+const {JWT_KEY} = require('../config/serverConfig');
+
+
+class UserService {
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async create(data) {
+        try {
+            const user = await this.userRepository.create(data);
+            return user;
+        } catch (error) {
+            console.log("Something went wrong in the service layer");
+            throw error;
+        }
+    }
+
+     createToken(user){
+        try {
+            const result = jwt.sign(user,JWT_KEY, {expiresIn : '1d' });
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+            
+        }
+    }
+
+     verifyToken(token){
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+            
+        } catch (error) {
+            console.log("Something went wrong in verification of token");
+            throw error;
+            
+        }
+     }
+}
+
+module.exports = UserService;
